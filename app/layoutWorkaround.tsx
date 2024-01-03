b0c524c856da6b9c9744180ca591dc978c3a654c
k-sauce <96152903+k-sauce@users.noreply.github.com>
2024-01-03 18:55:31 +0100

    (feat/transition): reverted to template usage

---
+++ app/layoutWorkaround.tsx
@@ -0,0 +1,52 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Providers } from './providers'
+import { getSandpackCssText } from '@codesandbox/sandpack-react'
+import SandCss from '@components/SandCss'
+import ClientLayout from './ClientLayout'
+import Navigation from '@components/Navigation'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'COBBLE2',
+  description: 'Generated with love',
+}
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className='dark h-full'>
+      <head>
+        <SandCss />
+      </head>
+      <body className={inter.className + ' ' + 'h-full'}>
+        <div className="w-full flex flex-col">
+          {/* <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        /> */}
+          <div className="h-1 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
+          {/* <Providers> */}
+            <Navigation />
+            <div className="p-4 grow flex flex-col">
+              <ClientLayout>
+                {children}
+              </ClientLayout>
+            </div>
+          {/* </Providers> */}
+        </div>
+      </body>
+    </html>
+  )
+}
--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,46 +1,63 @@
-import { motion, AnimatePresence } from 'framer-motion';
+'use client'
+import Navigation from '@components/Navigation';
+// import ThemeDropdown from '@components/ThemeDropdown';
+import FrozenRouter from '@components/FrozenRouter';
+// import { MonacoTheme } from '@constants/monacoThemes';
+// import defineTheme from '@lib/defineTheme';
+import { ElementRef, forwardRef, useEffect, useState } from "react";
+// import { ToastContainer } from 'react-toastify';
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  usePathname,
+  useSelectedLayoutSegment,
+  useSelectedLayoutSegments,
+} from "next/navigation";
 
-import { usePathname } from 'next/navigation';
-import { ReactNode } from 'react';
 
-type PageTransitionProps = {
-  children: ReactNode;
-}
+const Child = forwardRef<
+  ElementRef<typeof motion.div>,
+  { children: React.ReactNode }
+>((props, ref) => {
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 100 }}
+    >
+      <FrozenRouter>{props.children}</FrozenRouter>
+    </motion.div>
+  );
+});
 
-export default function PageTransition({ children }: PageTransitionProps) {
+Child.displayName = "Child";
+
+
+export default function PageTransition({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const segment = useSelectedLayoutSegment();
+  // const [theme, setTheme] = useState<MonacoTheme>('vs-dark');
 
-  const variants = {
-    initial: {
-      opacity: 0,
-      x: '100vw',
-    },
-    animate: {
-      opacity: 1,
-      x: 0,
-    },
-    exit: {
-      opacity: 0,
-      x: '-100vw',
-    },
-  }
+
+
+  // const handleThemeChange = (newValue: unknown) => {
+  //   const selectedTheme = newValue as MonacoTheme
+  //   console.log(newValue, ['light', 'vs-dark'].includes(selectedTheme));
+  //   // if (['light', 'vs-dark'].includes(selectedTheme)) {
+  //   // setTheme(selectedTheme);
+  //   // } else {
+  //   defineTheme(selectedTheme).then(() => setTheme(selectedTheme));
+  //   // }
+
+  // };
+
 
   return (
-    <AnimatePresence mode={'wait'}>
-      <motion.div
-        key={pathname}
-        initial="intial"
-        animate="animate"
-        exit="exit"
-        transition={{
-          type: 'tween',
-          duration: 0.5,
-        }}
-        variants={variants}
-        className=" w-full flex flex-col grow"
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+        <AnimatePresence mode="popLayout" initial={true}>
+          <Child key={segment}>
+            {children}
+          </Child>
+        </AnimatePresence>
   )
+
 }
