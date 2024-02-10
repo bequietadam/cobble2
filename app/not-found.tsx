f8ddab0fd703417a8f5099f362bb5c1304a1e2ff
bequietadam <96152903+k-sauce@users.noreply.github.com>
2024-02-10 20:05:40 +0100

    (release/auth): added not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge';
+
+export default async function NotFound() {
+    // ...
+    return (
+        <>
+            <h1>{`Sorry the page can't be found`}</h1>
+        </>
+    )
+}
