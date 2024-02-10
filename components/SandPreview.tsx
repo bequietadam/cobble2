2862916a19d3705457ad7130da3cbca5e96b8fdf
bequietadam <96152903+k-sauce@users.noreply.github.com>
2024-02-10 19:53:19 +0100

    (release/auth): first

--- components/SandPreview.tsx
+++ components/SandPreview.tsx
@@ -44,7 +44,6 @@ export default function SandPreview({ cobble, disabled }: SandPreviewProps) {
           />
         </SandpackLayout>
         <div className="absolute w-full h-full bg-black/20 top-0 backdrop-blur-sm hover:backdrop-blur-none hover:bg-transparent transition-all rounded-2xl border-3 border-white" >
-
         </div>
       </SandpackProvider>
     </>
